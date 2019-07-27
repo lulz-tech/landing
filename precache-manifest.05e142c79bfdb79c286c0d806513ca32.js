43987fd3bc6c4e4456f0f3167eb2818e1fc304e5
Alex Mubarakshin <muban96@mail.ru>
2019-07-27 19:34:33 +0300

    Updates

---
+++ precache-manifest.05e142c79bfdb79c286c0d806513ca32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9343abadb63863e54621457d74d99b2",
+    "url": "/landing/index.html"
+  },
+  {
+    "revision": "c65fee97ece7a7912242",
+    "url": "/landing/static/css/2.a317ec87.chunk.css"
+  },
+  {
+    "revision": "f24113a662298426979c",
+    "url": "/landing/static/css/main.829f0b54.chunk.css"
+  },
+  {
+    "revision": "c65fee97ece7a7912242",
+    "url": "/landing/static/js/2.7e5d8550.chunk.js"
+  },
+  {
+    "revision": "f24113a662298426979c",
+    "url": "/landing/static/js/main.225812a8.chunk.js"
+  },
+  {
+    "revision": "583ba90f5cdbe59afd63",
+    "url": "/landing/static/js/runtime~main.088e786a.js"
+  },
+  {
+    "revision": "4ee5092c8fb64798f46055d476f1b26f",
+    "url": "/landing/static/media/logo.4ee5092c.png"
+  }
+]);
